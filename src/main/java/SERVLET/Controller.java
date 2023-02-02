5a89a8e4ea221967c78de068542aa7279f9f86f3
Harsh Khatri <31386281+Ferreter@users.noreply.github.com>
2023-02-02 14:05:02 +0000

    Fixed Controller.java

--- src/main/java/SERVLET/Controller.java
+++ src/main/java/SERVLET/Controller.java
@@ -20,10 +20,12 @@ import javax.servlet.http.HttpSession;
  *
  * @author kian2
  */
-@WebServlet(name = "Controller", urlPatterns = {"/Controller"})
+@WebServlet(name = "Controller", urlPatterns =
+{
+    "/Controller"
+})
 public class Controller extends HttpServlet {
 
-    
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
      * methods.
@@ -34,126 +36,133 @@ public class Controller extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
         HttpSession session = request.getSession(true);
         String forwardToJsp = "index.jsp";
         String action = request.getParameter("action");
-        if (action != null) {
-            switch (action) {
+        if (action != null)
+        {
+            switch (action)
+            {
                 case "login":
                     forwardToJsp = LoginPage(request, response);
-                    
+
                     break;
-                    case "register":
+                case "register":
                     forwardToJsp = RegisterPage(request, response);
-                    response.sendRedirect(forwardToJsp);
                     break;
             }
+
+            response.sendRedirect(forwardToJsp);
         }
     }
+
     private String LoginPage(HttpServletRequest request, HttpServletResponse response) {
-      String forwardToJsp = "index.jsp";
+        String forwardToJsp = "index.jsp";
         HttpSession session = request.getSession(true);
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        if (username != null && password != null && !username.isEmpty() && !password.isEmpty()) {
+        if (username != null && password != null && !username.isEmpty() && !password.isEmpty())
+        {
             UserDao userDao = new UserDao("clothes_shop");
             user u = userDao.findUserByUsernamePassword(username, password);
-            
-            if (u == null) {
+
+            if (u == null)
+            {
                 forwardToJsp = "controller/index.jsp";
                 String error = "Incorrect credentials supplied. Please <a href=\"login.jsp\">try again.</a>";
                 session.setAttribute("errorMessage", error);
-            } else {
+            } else
+            {
                 forwardToJsp = "loginSuccessful.jsp";
                 session.setAttribute("username", username);
                 session.setAttribute("user", u);
             }
-        } else {
+        } else
+        {
             forwardToJsp = "error.jsp";
             String error = "No username and/or password supplied. Please <a href=\"login.jsp\">try again.</a>";
             session.setAttribute("errorMessage", error);
         }
         return forwardToJsp;
     }
+
     private String RegisterPage(HttpServletRequest request, HttpServletResponse response) {
-     String forwardToJsp = "controller/index.jsp";
+        String forwardToJsp = "controller/index.jsp";
         HttpSession session = request.getSession(true);
-         String firstname = request.getParameter("firstname");
+        String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
         String email = request.getParameter("email");
         String phone = request.getParameter("phone");
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String dob = request.getParameter("dob");
-        Date date=Date.valueOf(dob);
-        if (username != null && password != null && !username.isEmpty() && !password.isEmpty() ) {
+        Date date = Date.valueOf(dob);
+        if (username != null && password != null && !username.isEmpty() && !password.isEmpty())
+        {
             UserDao userDao = new UserDao("clothes_shop");
             user u = userDao.findUserByUsernamePassword(username, password);
             boolean login = false;
-            
-            if (u == null) {
+
+            if (u == null)
+            {
                 forwardToJsp = "controller/index.jsp";
                 session.setAttribute("username", username);
                 session.setAttribute("user", u);
-                user user = new user(username, password, firstname, lastname,email,phone,date);
+                user user = new user(username, password, firstname, lastname, email, phone, date);
                 login = userDao.addUser(user);
-            } else {
-               forwardToJsp = "view/error.jsp";
+            } else
+            {
+                forwardToJsp = "view/error.jsp";
                 String error = "user already exists <a href=\"register.jsp\">try again.</a>";
                 session.setAttribute("errorMessage", error);
             }
-        } else {
+        } else
+        {
             forwardToJsp = "view/error.jsp";
             String error = "No username and/or password and/or email and/or phone and/or firstname and/or lastname supplied. Please <a href=\"login.jsp\">try again.</a>";
             session.setAttribute("errorMessage", error);
         }
         return forwardToJsp;
-       }
+    }
 
-            // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-            /**
-             * Handles the HTTP <code>GET</code> method.
-             *
-             * @param request servlet request
-             * @param response servlet response
-             * @throws ServletException if a servlet-specific error occurs
-             * @throws IOException if an I/O error occurs
-             */
-            @Override
-            protected void doGet
-            (HttpServletRequest request, HttpServletResponse response)
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                processRequest(request, response);
-            }
+        processRequest(request, response);
+    }
 
-            /**
-             * Handles the HTTP <code>POST</code> method.
-             *
-             * @param request servlet request
-             * @param response servlet response
-             * @throws ServletException if a servlet-specific error occurs
-             * @throws IOException if an I/O error occurs
-             */
-            @Override
-            protected void doPost
-            (HttpServletRequest request, HttpServletResponse response)
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-                processRequest(request, response);
-            }
+        processRequest(request, response);
+    }
 
-            /**
-             * Returns a short description of the servlet.
-             *
-             * @return a String containing servlet description
-             */
-            @Override
-            public String getServletInfo
-            
-                () {
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
         return "Short description";
-            }// </editor-fold>
+    }// </editor-fold>
 
-        }
-
-    
+}
