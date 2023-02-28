9d499a5164a57475177da7a9a65e7cc343f981d2
kian-Harding <KianHarding7@hotmail.com>
2023-02-28 14:18:32 +0000

    admin login

--- src/main/java/sql/clothes_shop.sql
+++ src/main/java/sql/clothes_shop.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 15, 2023 at 11:13 AM
--- Server version: 10.4.20-MariaDB
--- PHP Version: 8.0.9
+-- Generation Time: Feb 23, 2023 at 11:25 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,53 +24,17 @@ drop database if exists clothes_shop;
 create database if not exists clothes_shop;
 
 use clothes_shop;
-
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bottoms`
+-- Table structure for table `cart`
 --
 
-CREATE TABLE `bottoms` (
-  `BottomsId` varchar(255) NOT NULL,
-  `Name` varchar(55) NOT NULL,
-  `Images` varchar(255) NOT NULL,
-  `MRP` double NOT NULL,
-  `Brand` varchar(55) NOT NULL,
-  `CPrice` double NOT NULL,
-  `Description` varchar(255) NOT NULL,
-  `Category` varchar(50) NOT NULL,
-  `Tags` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bottomsfilter`
---
-
-CREATE TABLE `bottomsfilter` (
-  `BottomsId` varchar(255) NOT NULL,
-  `Fit` varchar(50) NOT NULL,
-  `Colour` varchar(50) NOT NULL,
-  `Style` varchar(50) NOT NULL,
-  `Printed` varchar(50) NOT NULL,
-  `Occasion` varchar(50) NOT NULL,
-  `Length` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `bottomsstock`
---
-
-CREATE TABLE `bottomsstock` (
-  `BottomsId` varchar(255) NOT NULL,
-  `S` int(11) NOT NULL,
-  `M` int(11) NOT NULL,
-  `L` int(11) NOT NULL,
-  `XL` int(11) NOT NULL
+CREATE TABLE `cart` (
+  `UserId` int(11) NOT NULL,
+  `ProductId` varchar(255) NOT NULL,
+  `Quantity` int(255) NOT NULL,
+  `TotalPrice` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -91,45 +55,48 @@ CREATE TABLE `orders` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tops`
+-- Table structure for table `productfilter`
 --
 
-CREATE TABLE `tops` (
-  `TopsId` varchar(255) NOT NULL,
-  `Name` varchar(50) NOT NULL,
-  `Images` varchar(255) NOT NULL,
+CREATE TABLE `productfilter` (
+  `ProductId` varchar(255) NOT NULL,
+  `Colour` varchar(255) NOT NULL,
+  `Style` varchar(255) NOT NULL,
+  `Printed` varchar(255) NOT NULL,
+  `Length` varchar(255) NOT NULL,
+  `Occasion` varchar(255) NOT NULL,
+  `Fit` varchar(255) NOT NULL,
+  `NeckLine` varchar(255) NOT NULL,
+  `Material` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ProductId` varchar(255) NOT NULL,
+  `Name` varchar(255) NOT NULL,
   `MRP` double NOT NULL,
-  `Brand` varchar(50) NOT NULL,
-  `CPrice` double NOT NULL,
+  `CP` double NOT NULL,
   `Description` varchar(255) NOT NULL,
-  `Category` varchar(50) NOT NULL,
-  `Tags` varchar(50) NOT NULL
+  `Category` varchar(255) NOT NULL,
+  `Tags` varchar(255) NOT NULL,
+  `Images` varchar(255) NOT NULL,
+  `Brand` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `topsfilter`
+-- Table structure for table `stock`
 --
 
-CREATE TABLE `topsfilter` (
-  `TopsId` varchar(255) NOT NULL,
-  `Printed` varchar(50) NOT NULL,
-  `Fit` varchar(50) NOT NULL,
-  `NeckLine` varchar(50) NOT NULL,
-  `Colour` varchar(50) NOT NULL,
-  `Occasion` varchar(50) NOT NULL,
-  `Material` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `topsstock`
---
-
-CREATE TABLE `topsstock` (
-  `TopsId` varchar(255) NOT NULL,
+CREATE TABLE `stock` (
+  `ProductId` varchar(255) NOT NULL,
+  `XS` int(11) NOT NULL,
   `S` int(11) NOT NULL,
   `M` int(11) NOT NULL,
   `L` int(11) NOT NULL,
@@ -144,79 +111,58 @@ CREATE TABLE `topsstock` (
 
 CREATE TABLE `user` (
   `UserId` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `FirstName` varchar(50) NOT NULL,
-  `Lastname` varchar(50) NOT NULL,
-  `Email` varchar(50) NOT NULL,
-  `Phone` int(11) NOT NULL,
-  `DOB` date NOT NULL
+  `username` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `FirstName` varchar(55) DEFAULT NULL,
+  `Lastname` varchar(55) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Phone` int(11) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  `isAdmin` Boolean DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`UserId`, `username`, `password`, `FirstName`, `Lastname`, `Email`, `Phone`, `DOB`) VALUES
-(1, 'seany', 'password', 'Sean', 'Dyche', 'sean1@gmail.com', 875234283, '1954-10-26'),
-(2, 'deanSmt', 'password', 'Dean', 'Smith', 'deanSm@gmail.com', 84958266, '2000-04-08'),
-(3, 'DanOb', 'password', 'Dani', 'Obrien', 'danObr@gmail.com', 879582432, '2001-07-20'),
-(4, 'bigKhali', 'password', 'Meshach', 'Khali', 'meshKha@gmail.com', 84958266, '2002-10-08'),
-(5, 'ferreter', 'password', 'Harsh', 'Kathri', 'katHarsh@student.dkit.ie', 84958266, '1965-01-11'),
-(6, 'Kian', 'password', 'Kian', 'Harding', 'Kian123@gmail.com', 84958266, '2000-12-25'),
-(7, 'kian2ki', 'Password1', 'Kian', 'Harding', 'kian2ki@hotmail.com', 83, '2023-02-08');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `bottoms`
+-- Indexes for table `cart`
 --
-ALTER TABLE `bottoms`
-  ADD PRIMARY KEY (`BottomsId`);
-
---
--- Indexes for table `bottomsfilter`
---
-ALTER TABLE `bottomsfilter`
-  ADD KEY `BottomsId` (`BottomsId`);
-
---
--- Indexes for table `bottomsstock`
---
-ALTER TABLE `bottomsstock`
-  ADD KEY `BottomsId` (`BottomsId`);
+ALTER TABLE `cart`
+  ADD KEY `UserId` (`UserId`),
+  ADD KEY `ProductId` (`ProductId`);
 
 --
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD KEY `UserId` (`UserId`);
+  ADD KEY `orders_ibfk_1` (`UserId`);
 
 --
--- Indexes for table `tops`
+-- Indexes for table `productfilter`
 --
-ALTER TABLE `tops`
-  ADD PRIMARY KEY (`TopsId`);
+ALTER TABLE `productfilter`
+  ADD KEY `ProductId` (`ProductId`);
 
 --
--- Indexes for table `topsfilter`
+-- Indexes for table `products`
 --
-ALTER TABLE `topsfilter`
-  ADD KEY `TopsId` (`TopsId`);
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductId`);
 
 --
--- Indexes for table `topsstock`
+-- Indexes for table `stock`
 --
-ALTER TABLE `topsstock`
-  ADD KEY `TopsId` (`TopsId`);
+ALTER TABLE `stock`
+  ADD KEY `ProductId` (`ProductId`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`UserId`);
+  ADD PRIMARY KEY (`UserId`),
+  ADD UNIQUE KEY `UsernameIndex` (`username`),
+  ADD UNIQUE KEY `Email Index` (`Email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -226,23 +172,18 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `UserId` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `bottomsfilter`
+-- Constraints for table `cart`
 --
-ALTER TABLE `bottomsfilter`
-  ADD CONSTRAINT `bottomsfilter_ibfk_1` FOREIGN KEY (`BottomsId`) REFERENCES `bottoms` (`BottomsId`);
-
---
--- Constraints for table `bottomsstock`
---
-ALTER TABLE `bottomsstock`
-  ADD CONSTRAINT `bottomsstock_ibfk_1` FOREIGN KEY (`BottomsId`) REFERENCES `bottoms` (`BottomsId`);
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserId`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`ProductId`) REFERENCES `products` (`ProductId`);
 
 --
 -- Constraints for table `orders`
@@ -251,16 +192,16 @@ ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserId`);
 
 --
--- Constraints for table `topsfilter`
+-- Constraints for table `productfilter`
 --
-ALTER TABLE `topsfilter`
-  ADD CONSTRAINT `topsfilter_ibfk_1` FOREIGN KEY (`TopsId`) REFERENCES `tops` (`TopsId`);
+ALTER TABLE `productfilter`
+  ADD CONSTRAINT `productfilter_ibfk_1` FOREIGN KEY (`ProductId`) REFERENCES `products` (`ProductId`);
 
 --
--- Constraints for table `topsstock`
+-- Constraints for table `stock`
 --
-ALTER TABLE `topsstock`
-  ADD CONSTRAINT `topsstock_ibfk_1` FOREIGN KEY (`TopsId`) REFERENCES `tops` (`TopsId`);
+ALTER TABLE `stock`
+  ADD CONSTRAINT `stock_ibfk_1` FOREIGN KEY (`ProductId`) REFERENCES `products` (`ProductId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
