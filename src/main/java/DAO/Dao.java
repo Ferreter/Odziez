df3a19f64c4640ef0245a35fa33d1b098c373b15
Harsh Khatri <31386281+Ferreter@users.noreply.github.com>
2023-03-21 14:41:01 +0000

    Made properties file

--- src/main/java/DAO/Dao.java
+++ src/main/java/DAO/Dao.java
@@ -1,8 +1,13 @@
 package DAO;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import java.util.ResourceBundle;
 
 public class Dao {
 
@@ -26,24 +31,43 @@ public class Dao {
      */
     public Connection getConnection() {
 
-        String driver = "com.mysql.cj.jdbc.Driver";
-        String url = "jdbc:mysql://localhost:3306/" + databaseName;
-        String username = "root";
-        String password = "";
         Connection con = null;
-        try
+        try ( InputStream input = new FileInputStream("config.properties"))
         {
-            Class.forName(driver);
-            con = DriverManager.getConnection(url, username, password);
-        } catch (ClassNotFoundException ex1)
+
+            Properties prop = new Properties();
+
+            // load a properties file
+            prop.load(input);
+
+            // get the property value and print it out
+            System.out.println(prop.getProperty("db.url"));
+            System.out.println(prop.getProperty("db.user"));
+            System.out.println(prop.getProperty("db.password"));
+
+            String driver = "com.mysql.cj.jdbc.Driver";
+            String url = "jdbc:mysql://localhost:3306/" + databaseName;
+            String username = "root";
+            String password = "";
+            try
+            {
+                Class.forName(driver);
+                con = DriverManager.getConnection(url, username, password);
+            } catch (ClassNotFoundException ex1)
+            {
+                System.out.println("Failed to find driver class " + ex1.getMessage());
+                System.exit(1);
+            } catch (SQLException ex2)
+            {
+                System.out.println("Connection failed " + ex2.getMessage());
+                System.exit(2);
+            }
+
+        } catch (IOException ex)
         {
-            System.out.println("Failed to find driver class " + ex1.getMessage());
-            System.exit(1);
-        } catch (SQLException ex2)
-        {
-            System.out.println("Connection failed " + ex2.getMessage());
-            System.exit(2);
+            ex.printStackTrace();
         }
+
         return con;
     }
 
@@ -52,8 +76,8 @@ public class Dao {
      * Releases the specified {@link java.sql.Connection} object and makes it
      * available for reuse.
      *
-     * @param con the {@link java.sql.Connection} object to release
-     * catches SQLException if a database access error occurs
+     * @param con the {@link java.sql.Connection} object to release catches
+     * SQLException if a database access error occurs
      */
     public void freeConnection(Connection con) {
         try
@@ -72,7 +96,7 @@ public class Dao {
 
     /**
      * Returns the string with database name in it
-     * 
+     *
      * @return a string containing name of database
      */
     public String getDatabaseName() {
